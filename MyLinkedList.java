d1ca2aa1827edfd6df295b45bc8ef518a57d4451
wlin10 <wlin10@stuy.edu>
2021-01-08 11:32:06 -0500

    get

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -7,10 +7,29 @@ public class MyLinkedList{
  public int size() {
    return this.size;
  }
- public boolean add(String value);
- public boolean add(int index, String value);
- public String get(int index);
- public String set(int index, String value);
- public String toString();
+ public boolean add(String value) {
+   return true;
+}
+ public void add(int index, String value) {
+
+ }
+ public String get(int index) {
+   if (index < 0 || index >= size()) {
+     throw new IndexOutOfBoundsException("index " + index + " is out of range");
+   }
+   Node ans = start;
+   for (int i = 0; i < size(); i++) {
+     while (i != index) {
+       ans = start.getNext();
+     }
+   }
+   return ans.getData();
+ }
+ public String set(int index, String value) {
+   return "";
+ }
+ public String toString() {
+   return "";
+ }
  //Any helper method that returns a Node object MUST BE PRIVATE!
 }
