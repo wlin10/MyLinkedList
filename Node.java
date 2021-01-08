e6e456398755a862b55f93a9ea80b27bcae913e9
wlin10 <wlin10@stuy.edu>
2021-01-08 10:23:02 -0500

    get,set,constructor

--- Node.java
+++ Node.java
@@ -1,6 +1,28 @@
 public class Node{
- public Node(String value){/*create a constructor*/}
+ public Node(String value){
+   this.data = value;
+ }
  private String data;
  private Node next,prev;
  //write get/set methods for all three instance variables.
+
+ public void setData(String data) {
+   this.data = data;
+ }
+ public String getData() {
+   return data;
+ }
+ public void setNext(Node next) {
+   this.next = next;
+ }
+ public Node getNext() {
+   return next;
+ }
+ public void setPrev(Node prev) {
+   this.prev = prev;
+ }
+ public Node getPrev() {
+   return prev;
+ }
+
 }
